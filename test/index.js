6a2524a774b338bf914c04c3327effadd20da568
dmonego <dmonego@gmail.com>
2018-11-17 12:13:27 -0500

    call with babel-node

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-const {rules} = require("eslint-plugin-richtext-cp");
-const {RuleTester} = require("eslint");
+var {rules} = require("eslint-plugin-richtext-cp");
+var {RuleTester} = require("eslint");
 
 RuleTester.setDefaultConfig({
     parser: "babel-eslint",
